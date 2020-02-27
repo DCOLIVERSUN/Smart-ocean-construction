30d2189dbbdd6cac0d1c3e0c67e041921100ae40
OliverSun <sunqian18s@ict.ac.cn>
2020-02-27 14:37:22 +0800

    按照 TODO 修改为提交版本

--- code/main.py
+++ code/main.py
@@ -124,8 +124,8 @@ def feature_engineer(df, flag=True):
 # TODO
 # 先用处理好的数据
 #data_path = r'./'
-train_data = pd.read_csv('trian_data_0227_21.csv', header = 0)
-train_data.drop(['high_lon_ratio'], axis=1,inplace=True)
+#train_data = pd.read_csv('trian_data_0227_21.csv', header = 0)
+#train_data.drop(['x_3/4','high_lon_ratio'], axis=1,inplace=True)
 ##########
 # 处理训练集
             
@@ -134,23 +134,23 @@ features = []
 train_path = r'./tcdata/hy_round2_train_20200225'
 #train_path = r'../data/hy_round2_train_20200225'
 # TODO: 后续需要取消注释
-#train_files = os.listdir(train_path)
-#train_files_len = len(train_files)
-#print("The len of train is " + str(train_files_len))
-#
-#for file in tqdm(train_files):
-#    df = pd.read_csv(os.path.join(train_path, file), header=0, keep_default_na=False)
-#    feature_engineer(df, flag=True)
-#
-#train_data = pd.DataFrame(np.array(features).reshape(train_files_len, int(len(features) / train_files_len)))
-#train_data.columns = ['x_min','x_max','x_mean','x_1/4', 'x_1/2', 'x_3/4',
-#                     'y_min','y_max','y_mean','y_3/4',
-#                     'xy_cov',
-#                     'a',
-#                     'v_mean','v_std','v_3/4',
-#                     'd_mean', 'static_ratio', 'medium_v_ratio',
-#                     'low_lon_ratio', 'medium_lon_ratio', 'high_lon_ratio',
-#                     'type']
+train_files = os.listdir(train_path)
+train_files_len = len(train_files)
+print("The len of train is " + str(train_files_len))
+
+for file in tqdm(train_files):
+    df = pd.read_csv(os.path.join(train_path, file), header=0, keep_default_na=False)
+    feature_engineer(df, flag=True)
+
+train_data = pd.DataFrame(np.array(features).reshape(train_files_len, int(len(features) / train_files_len)))
+train_data.columns = ['x_min','x_max','x_mean','x_1/4', 'x_1/2', 
+                     'y_min','y_max','y_mean','y_3/4',
+                     'xy_cov',
+                     'a',
+                     'v_mean','v_std','v_3/4',
+                     'd_mean', 'static_ratio', 'medium_v_ratio',
+                     'low_lon_ratio', 'medium_lon_ratio', 
+                     'type']
 ## TODO：提交前删掉
 #train_data.to_csv('trian_data_0227_21.csv', index = None)
 ##########
@@ -160,8 +160,8 @@ train_path = r'./tcdata/hy_round2_train_20200225'
 
 features = []
 # TODO: 测试路径名后续需要改回
-#test_path = r'./tcdata/hy_round2_testA_20200225'
-test_path = r'../data/hy_round2_testA_20200225'
+test_path = r'./tcdata/hy_round2_testA_20200225'
+#test_path = r'../data/hy_round2_testA_20200225'
 test_files = os.listdir(test_path)
 test_files_len = len(test_files)
 print("The len of test is " + str(test_files_len))
