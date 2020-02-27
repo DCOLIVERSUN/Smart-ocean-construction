6da43c7a84955afb9dd5459bbb53200bff406de4
OliverSun <sunqian18s@ict.ac.cn>
2020-02-27 10:26:50 +0800

    增加 x_3/4 特征

--- code/main.py
+++ code/main.py
@@ -61,7 +61,7 @@ def feature_engineer(df, flag=True):
     features.append(df['lat'].mean())             #x_mean
     features.append(df['lat'].quantile(0.25))     #x_1/4
     features.append(df['lat'].quantile(0.5))      #x_1/2
-    features.append(df)
+    features.append(df['lat'].quantile(0.75))     #x_3/4
     
     features.append(df['lon'].min())              #y_min
     features.append(df['lon'].max())              #y_max
