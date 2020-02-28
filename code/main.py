7625154d2bb54cebd2f0b4462cc6c10bbc9c275f
OliverSun <sunqian18s@ict.ac.cn>
2020-02-28 14:45:18 +0800

    更改纬度状态范围

--- code/main.py
+++ code/main.py
@@ -67,11 +67,11 @@ def del_lon(df):
 def del_lat(df):
     df['lat_stage'] = 0
     for i in range(len(df)):
-        if 22 <= df['lat'].iloc[i] < 24:
+        if 20 <= df['lat'].iloc[i] < 24:
             df['lat_stage'].iloc[i] = 1
         elif 24 <= df['lat'].iloc[i] < 26:
             df['lat_stage'].iloc[i] = 2
-        elif 26 <= df['lat'].iloc[i] <= 28:
+        elif 26 <= df['lat'].iloc[i]:
             df['lat_stage'].iloc[i] = 3
     return df
 ###########
